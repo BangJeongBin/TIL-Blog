293060d2cd1a9591d2b3009af30d52b7ac55e655
BangJeongBin <bangjungbin@gmail.com>
2025-08-13 19:46:39 +0900

    사이드바 이미지 속성 추가

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755079565',resources: [
+    cacheName: 'chirpy-1755081973',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
