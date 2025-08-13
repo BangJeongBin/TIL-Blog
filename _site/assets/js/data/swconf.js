3b390468ed2ccc8b5029c854098fcb9898f281ca
BangJeongBin <bangjungbin@gmail.com>
2025-08-13 18:51:37 +0900

    사이드 바 이미지 변경 / 더미 페이지 생성

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755075380',resources: [
+    cacheName: 'chirpy-1755078559',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
