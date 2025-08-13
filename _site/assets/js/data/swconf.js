6cf34669d59283effce3712aef3ad136d021420e
BangJeongBin <bangjungbin@gmail.com>
2025-08-13 15:34:35 +0900

    Modift yml

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755065283',resources: [
+    cacheName: 'chirpy-1755066514',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
