1cdaced58a4e9fe438753426fe1751873a2b2169
BangJeongBin <bangjungbin@gmail.com>
2025-08-13 17:33:51 +0900

    아바타, 파비콘 변경 / 페이지네이션 확인용 게시물 추가

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755066514',resources: [
+    cacheName: 'chirpy-1755073945',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
