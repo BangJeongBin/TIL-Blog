92f6d96432d8324acf9b075eb66493f919c26263
BangJeongBin <bangjungbin@gmail.com>
2025-08-19 21:42:23 +0900

    media-url.html 원상복귀

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755606569',resources: [
+    cacheName: 'chirpy-1755607321',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
