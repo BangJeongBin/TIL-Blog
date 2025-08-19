9a28f5ffc80abf68fb34a53de8575a150848b72e
BangJeongBin <bangjungbin@gmail.com>
2025-08-19 20:37:19 +0900

    이미지 경로 수정 7

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755602744',resources: [
+    cacheName: 'chirpy-1755603407',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
