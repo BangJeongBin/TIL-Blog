039e5ce2343336753c5148f4f0ab0fe71970fc64
BangJeongBin <bangjungbin@gmail.com>
2025-08-19 22:11:07 +0900

    cdn 경로 삭제

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755607321',resources: [
+    cacheName: 'chirpy-1755609038',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
