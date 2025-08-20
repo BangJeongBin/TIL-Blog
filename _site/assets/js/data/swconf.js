edd3b1f014ecc0ea3c17b52c45ae115c06fa180b
BangJeongBin <bangjungbin@gmail.com>
2025-08-20 18:11:19 +0900

    media-url.html 수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755680002',resources: [
+    cacheName: 'chirpy-1755681061',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
