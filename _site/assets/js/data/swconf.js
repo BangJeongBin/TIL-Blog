08f0906df96a3ed748ae040d2de7202c4db14015
BangJeongBin <bangjungbin@gmail.com>
2025-12-11 17:03:26 +0900

    update post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1765433763',resources: [
+    cacheName: 'chirpy-1765440164',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
