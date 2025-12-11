c257e6660e1328acb972113572f5ad089d8b62eb
BangJeongBin <bangjungbin@gmail.com>
2025-12-11 15:16:20 +0900

    modify dir

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1765302449',resources: [
+    cacheName: 'chirpy-1765433763',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
