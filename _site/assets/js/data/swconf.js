c87b1f44884524db03918d66384cf467a9a48710
BangJeongBin <bangjungbin@gmail.com>
2025-12-21 17:49:00 +0900

    add post 2025-12-21-202551-security-issue-report.md

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1766306314',resources: [
+    cacheName: 'chirpy-1766306940',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
