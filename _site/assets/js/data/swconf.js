a8ca2aa0add5ca70a245cedd31501cf398f522d7
BangJeongBin <bangjungbin@gmail.com>
2026-01-07 21:19:37 +0900

    add post 2026-01-04-202601-security-issue-report.md

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1767787859',resources: [
+    cacheName: 'chirpy-1767788376',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
