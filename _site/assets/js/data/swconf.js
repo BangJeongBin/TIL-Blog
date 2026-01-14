4e095eee407718dcf045f9eadce0d20d27a4b383
BangJeongBin <bangjungbin@gmail.com>
2026-01-14 16:23:21 +0900

    Fix typos and update HTTP status codes post
    Corrected a stray character and improved formatting in the HTTP response status codes TIL post. Also fixed a minor typo in the Wireshark post and updated generated site files to reflect these content changes.

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1768361564',resources: [
+    cacheName: 'chirpy-1768375356',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
