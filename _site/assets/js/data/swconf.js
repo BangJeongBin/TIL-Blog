924790e1b7b62135c253cc1c970605606ffba16a
BangJeongBin <bangjungbin@gmail.com>
2026-01-20 16:37:59 +0900

    Update images and metadata, remove review logo
    Updated note-taking-logo.png and .DS_Store files, removed review-logo-1.png from assets. Also updated published/modified timestamps and cache name in generated site files.

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1768894294',resources: [
+    cacheName: 'chirpy-1768894674',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
